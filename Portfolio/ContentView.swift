d14967af010658aed101552fd4648a2ddb6606df
Rob Copping <robbiecop96@gmail.com>
2021-01-06 15:23:01 +0000

    Initial commit

---
+++ Portfolio/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Portfolio
+//
+//  Created by Rob Copping on 04/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView{
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            ProjectsView(showClosedProjects: false)
+                .tabItem {
+                    Image(systemName: "list.bullet")
+                    Text("Open Projects")
+                }
+            ProjectsView(showClosedProjects: true)
+                .tabItem {
+                    Image(systemName: "list.bullet")
+                    Text("Closed Projects")
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var dataController = DataController.preview
+    static var previews: some View {
+        ContentView()
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
+    }
+}
---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,26 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Rob Copping on 04/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    @StateObject var dataController: DataController
+    
+    init(){
+        let dataController = DataController()
+        _dataController = StateObject(wrappedValue: dataController)
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
+        }
+    }
+}
